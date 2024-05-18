ff2653e50fb3d6334f794082431893c519746399
daskie911 <imevil2302@gmail.com>
2024-05-18 20:59:42 +0200

    chore: Add mongoose dependency and update server start code

---
+++ README.md
@@ -0,0 +1,42 @@
+# Shop
+
+This is a simple e-commerce shop project.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/daskie911/shop.git
+   ```
+
+2. Install the dependencies:
+
+   ```bash
+   npm install
+   ```
+
+## Usage
+
+1. Start the development server:
+
+   ```bash
+   npm start
+   ```
+
+2. Open your browser and navigate to `http://localhost:3000` to view the shop.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const session = require("express-session");
 require("dotenv").config();
 
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const homeRoutes = require("./routes/homeRoutes");
 
@@ -21,6 +22,16 @@ app.use(express.static(__dirname + "/views"));
 
 app.use("/", homeRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect(`${process.env.DB_URL}`);
+    console.log("Connected to the database");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
