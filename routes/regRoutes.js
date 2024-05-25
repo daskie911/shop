aac17e840011efa3477d14ceb236d00e3d0cf6dd
daskie911 <imevil2302@gmail.com>
2024-05-25 20:38:05 +0200

    feat: Add validation for existing username and email in registration form

--- routes/regRoutes.js
+++ routes/regRoutes.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("reg", { error: "" });
+  res.render("reg", { error: "", username: "", email: "", password: "" });
 });
 
 // create a new user
@@ -12,18 +12,38 @@ router.post("/", async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
+    // checking if the username already exists
+    const userExists = await User.findOne({ username });
+    if (userExists) {
+      return res.render("reg", {
+        error: "This username already exist",
+        username,
+        email,
+        password,
+      });
+    }
+    // checking if the email already exists
+    const emialExists = await User.findOne({ email });
+    if (emialExists) {
+      return res.render("reg", {
+        error: "This email already exist",
+        username,
+        email,
+        password,
+      });
+    }
+
+    if (password.length < 6) {
+      return res.render("reg", {
+        error: "Password should be at least 6 characters",
+        username,
+        email,
+        password,
+      });
+    }
+
     // hash the password
     const hashedPassword = await bcrypt.hash(password, 12);
-
-    // checking if the user already exists
-
-    const userExists = await User.findOne({ username, email });
-    if (userExists) {
-      return res.render("reg", { error: "User already exists" });
-    }
-    if (password.length < 12) {
-      return res.render("reg", { error: "Password should be at least 12 characters" });
-    }
 
     const user = new User({
       username,
@@ -36,10 +56,6 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-});
-
-router.get("/", (req, res) => {
-  res.render("reg");
 });
 
 module.exports = router;
