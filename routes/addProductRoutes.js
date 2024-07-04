a1058a71f0975ba9c8f9b025410e475168459e82
daskie911 <imevil2302@gmail.com>
2024-07-05 00:57:37 +0200

    added some css and also make showig prodcut when u adding it on admin page but only for rn

--- routes/addProductRoutes.js
+++ routes/addProductRoutes.js
@@ -2,16 +2,18 @@ const { Router } = require("express");
 const Product = require("../models/Product");
 const router = new Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   try {
     if (!req.session.admin) {
       return res.redirect("/");
     }
 
+    const products = await Product.find(); 
     const url = process.env.URL_ADMIN;
-    res.render("addProduct", { error: "", url });
+    res.render("addProduct", { error: "", url, products }); 
   } catch (error) {
     console.log(error);
+    return res.status(500).send("Internal Server Error");
   }
 });
 
